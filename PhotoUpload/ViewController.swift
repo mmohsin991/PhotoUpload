45205a28e24aa750be1f74926e3fee76ba1324fc
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-03-08 00:39:48 +0500

    update

--- PhotoUpload/ViewController.swift
+++ PhotoUpload/ViewController.swift
@@ -15,12 +15,16 @@ import UIKit
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var myImageView: UIImageView!
-    let picker = UIImagePickerController()   //our controller.
-    //Memory will be conserved a bit if you place this in the actions.
-    // I did this to make code a bit more streamlined
+    let picker = UIImagePickerController()
     
-    //MARK: - Methods
-    // An alert method using the new iOS 8 UIAlertController instead of the deprecated UIAlertview
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self   //the required delegate to get a photo back to the app.
+    }
+    
     // make the alert with the preferredstyle .Alert, make necessary actions, and then add the actions.
     // add to the handler a closure if you want the action to do anything.
     
@@ -31,13 +35,12 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         presentViewController(alertVC, animated: true, completion: nil)
     }
     
-    //MARK: - Actions
     //get a photo from the library. We present as a popover on iPad, and fullscreen on smaller devices.
     @IBAction func photoFromLibrary(sender: UIBarButtonItem) {
-        picker.allowsEditing = false //2
-        picker.sourceType = .PhotoLibrary //3
+        picker.allowsEditing = false
+        picker.sourceType = .PhotoLibrary
         picker.modalPresentationStyle = .Popover
-        presentViewController(picker, animated: true, completion: nil)//4
+        presentViewController(picker, animated: true, completion: nil)
         picker.popoverPresentationController?.barButtonItem = sender
     }
     
@@ -52,18 +55,13 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             noCamera()
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        picker.delegate = self   //the required delegate to get a photo back to the app.
-    }
-    //MARK: - Delegates
+
     //What to do when the picker returns with a photo
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        var chosenImage = info[UIImagePickerControllerOriginalImage] as UIImage //2
-        myImageView.contentMode = .ScaleAspectFit //3
-        myImageView.image = chosenImage //4
-        dismissViewControllerAnimated(true, completion: nil) //5
+        var chosenImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        myImageView.contentMode = .ScaleAspectFit
+        myImageView.image = chosenImage
+        dismissViewControllerAnimated(true, completion: nil)
     }
     //What to do if the image picker cancels.
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
